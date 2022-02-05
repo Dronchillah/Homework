37acaee938ee59d0a056c457a12737816c8fa674
Drocnhillah <pizzamaker228@gmail.com>
2022-02-05 17:16:54 +0200

    finished binary to octal converter

--- src/main/java/com/dron/lessons/lesson1/LessonOneMain.java
+++ src/main/java/com/dron/lessons/lesson1/LessonOneMain.java
@@ -8,6 +8,7 @@ public class LessonOneMain {
     public static void main(String[] args) {
         String[] binaryValues = {"0000000110011001", "0011100011001000", "0011001001001101", "0000110000000101", "0011111010111110", "0111001011111000", "0011001100111001"};
         System.out.println(Arrays.toString(Converter.binaryToDecimal(binaryValues)));
+        System.out.println(Arrays.toString(Converter.binaryToOctal(binaryValues)).replaceAll("0", ""));
 
     }
 
